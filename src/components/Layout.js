e9f1e79ccfe6f4eec12b9b559d95ba3892eefb46
Mack Solomon <msolomon@eharmony.com>
2019-01-08 11:01:53 -0800

    center

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -56,7 +56,8 @@ class Layout extends React.Component {
       <div
         style={{
           maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          margin: '0 auto'
         }}
       >
         {header}
