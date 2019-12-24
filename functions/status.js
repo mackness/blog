96b60658e8b0d46b3fb160a67cbf19b62e07ef0b
Mack Solomon <macksol@gmail.com>
2019-12-24 09:07:37 -0800

    value query param

--- functions/status.js
+++ functions/status.js
@@ -1,10 +1,9 @@
-const { version } = require('../package.json')
-
 exports.handler = function(event, context, callback) {
+  const value = event.queryStringParameters.value || 42
   callback(null, {
     statusCode: 200,
     body: {
-      version,
+      value,
     },
   })
 }
